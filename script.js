dfb0cdf960fc182a23772a2ab041089e23ed4068
May <no-reply>
2021-09-25 19:32:28 -0700

    added a bunch of JS variables that i'm going to use and added start button

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+//this is my variable bank, where I store my precious variables
+var startButton = document.querySelector("#startButton");
+var answerButton = document.querySelectorAll("button");
+var answers = document.querySelector("#answers");
+var content = document.querySelector("#content");
+var questonNumber = document.querySelector("#top")
+
