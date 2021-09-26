5ae2a26a0b1134cf2cfe81d21a43e440a8ac8c0e
May <no-reply>
2021-09-26 00:12:29 -0700

    edited css and got it working, added scoreboard

--- script.js
+++ script.js
@@ -1,45 +1,38 @@
 
 //this is my variable bank, where I store my precious variables
-var startButton = document.querySelector("#startButton");
-var answerButton = document.querySelectorAll("button");
-var answers = document.querySelector("#answers");
-var content = document.querySelector("#content");
-var questonNumber = document.querySelector("#top")
-var timer =document.querySelector("#timer")
+var startButton     = document.querySelector("#startButton");
+var answerButton    = document.querySelectorAll("button");
+var answers         = document.querySelector("#answers");
+var content         = document.querySelector("#content");
+var questonNumber   = document.querySelector("#top")
+var timer           = document.querySelector("#timer")
 
 
 
-//-------------------------------------------------------
-function countdown() {
-    var timeLeft = 60;
-var timer = setInterval(function () {
-    if (timeLeft > 0) {
-        timer.textContent = timeLeft;
-        timerLeft--;
-    } else {
-        timer.textContent = "";
-        console.log(TIMERrANoUT);
-        clearInterval(timer);
-    }
-    },1000);
-};
-
-
-questions = {
-    questionNumber
-}
-
-
-
-
+//------------------------------------------------------------------//
+//countdown function using set interval and clear interval          //
+// timer.textContent = 0;
 
 startButton.addEventListener("click", function() {
+    var timeLeft = 60;
+    var timer = setInterval(function () {
+        if (timeLeft > 0) {
+            timer.textContent = timeLeft;
+            timerLeft--;
+        } else {
+            timer.textContent = "";
+            console.log(TIMERrANoUT);
+            clearInterval(timer);
+        }
+    },1000);
+});
+//------------------------------------------------------------------//
+
+// questions = {
+//     questionNumber
+// }
 
 
 
 
 
-
-
-
-})
